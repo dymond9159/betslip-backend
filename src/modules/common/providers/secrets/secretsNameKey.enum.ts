33436e811f4e942eeda2a54c7848f7f0f5c83340
fullstack_dev <dongying9159@outlook.com>
2025-02-25 07:54:52 -0700

    feat(common): add filters, decorators, providers, and validators

---
+++ src/modules/common/providers/secrets/secretsNameKey.enum.ts
@@ -0,0 +1,9 @@
+export enum EnumSecretsNameKey {
+    JwtSecretKey = "JWT_SECRET_KEY",
+    SecretA = "/path/to:SecretA",
+    SecretB = "/path/to:SecretB",
+    Common = "/path/to/secrets/common",
+    SecretA = "SecretA:1",
+    SecretB = "SecretB:2",
+    SecretC = "SecretC:3"
+}
